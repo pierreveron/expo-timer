baac5092d1f98a6e0130bbc2073c70922582123d
Pierre Véron <pierre.veron@outlook.fr>
2021-11-05 22:23:11 +0000

    fix glitch when opening screens by fading views

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,13 +4,16 @@ import { Ionicons } from "@expo/vector-icons";
 import FontInter from "../constants/FontInter";
 import { SharedElement } from "react-navigation-shared-element";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Animated } from "react-native";
 
 export default function Header({
   title,
   onPress,
+  fadeAnim,
 }: {
   title: string;
   onPress: () => void;
+  fadeAnim: Animated.Value;
 }) {
   const iconWidth = 40;
   const { top: insetsTop } = useSafeAreaInsets();
@@ -25,16 +28,16 @@ export default function Header({
         zIndex: 1,
       }}
     >
-      <Ionicons
-        name="arrow-back-circle"
-        size={iconWidth}
-        color="white"
-        onPress={onPress}
-        style={{
-          position: "absolute",
-          left: 0,
-        }}
-      />
+      <Animated.View
+        style={{ opacity: fadeAnim, position: "absolute", left: 0 }}
+      >
+        <Ionicons
+          name="arrow-back-circle"
+          size={iconWidth}
+          color="white"
+          onPress={onPress}
+        />
+      </Animated.View>
       <SharedElement id={title}>
         <Text
           className="text-white text-center text-2xl"
--- components/ScreenWrapper.tsx
+++ components/ScreenWrapper.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Header from "./Header";
-import { ColorValue } from "react-native";
+import { Animated, ColorValue } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import SharedBackground from "./SharedBackgound";
 
@@ -15,11 +15,49 @@ export default function ScreenWrapper({
   backgroundColor: ColorValue;
   children?: React.ReactNode;
 }) {
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+  const fadeInDuration = 400;
+  const fadeOutDuration = 80;
+
+  const fadeIn = () => {
+    // Will change fadeAnim value to 1 in 0.3 seconds
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: fadeInDuration,
+      useNativeDriver: false,
+    }).start();
+  };
+
+  const fadeOut = () => {
+    // Will change fadeAnim value to 0 in 0.1 seconds
+    Animated.timing(fadeAnim, {
+      toValue: 0,
+      duration: fadeOutDuration,
+      useNativeDriver: false,
+    }).start();
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      fadeIn();
+    }, fadeInDuration);
+  }, []);
   return (
     <SafeAreaView edges={["bottom", "left", "right"]} style={{ flex: 1 }}>
       <SharedBackground title={title} backgroundColor={backgroundColor} />
-      <Header title={title} onPress={onPress} />
-      {children}
+      <Header
+        title={title}
+        onPress={() => {
+          fadeOut();
+          setTimeout(() => {
+            onPress();
+          }, fadeOutDuration);
+        }}
+        fadeAnim={fadeAnim}
+      />
+      <Animated.View style={{ flex: 1, opacity: fadeAnim }}>
+        {children}
+      </Animated.View>
     </SafeAreaView>
   );
 }
