657d51ebdb64a634cba18f2c196550da5d03d4a0
Pierre Véron <pierre.veron@outlook.fr>
2021-11-15 22:52:04 +0100

    timerScreen with input

--- screens/ClassicTimerScreen.tsx
+++ screens/ClassicTimerScreen.tsx
@@ -1,18 +1,102 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useRef, useState } from "react";
+import { Dimensions, Keyboard, StyleSheet, TextInput } from "react-native";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 import Header from "../components/Header";
 import ScreenWrapper from "../components/ScreenWrapper";
+import { View, Text } from "../components/tailwind";
 import Colors from "../constants/Colors";
+import FontInter from "../constants/FontInter";
+import useTimer from "../hooks/useTimer";
 import { RootStackScreenProps } from "../types";
+import {
+  formatTime,
+  formatTimeNumber,
+  formatTimeString,
+  reformatTimeNumber,
+} from "../utils/time";
 
 export default function ClassicTimerScreen({
   navigation,
 }: RootStackScreenProps<"ClassicTimer">) {
+  const [number, setNumber] = useState<number>(60);
+
+  const textInput = useRef<TextInput>(null);
+
+  const onChangeText = (text: string) => {
+    setNumber(Number(text));
+    if (text.length == 4) {
+      Keyboard.dismiss();
+      setNumber(reformatTimeNumber(Number(text)));
+    }
+  };
+
+  const onTextClick = () => {
+    if (textInput.current) {
+      if (textInput.current.isFocused()) {
+        Keyboard.dismiss();
+        setNumber(reformatTimeNumber(number));
+      } else {
+        setNumber(0);
+        textInput.current.focus();
+      }
+    }
+  };
+
+  const onScreenClick = () => {
+    if (textInput.current) {
+      if (textInput.current.isFocused()) {
+        Keyboard.dismiss();
+        setNumber(reformatTimeNumber(number));
+      }
+    }
+  };
+
+  const {
+    timer,
+    isActive,
+    isPaused,
+    handleStart,
+    handlePause,
+    handleResume,
+    handleReset,
+    handleCancel,
+  } = useTimer(number);
+
   return (
     <ScreenWrapper
       title="Classic Timer"
       onPress={() => navigation.navigate("Home")}
       backgroundColor={Colors.classicTimer}
-    ></ScreenWrapper>
+    >
+      <TouchableWithoutFeedback
+        onPress={onScreenClick}
+        // accessible={false}
+        // style={{ flex: 1 }}
+      >
+        <View className="h-full items-center justify-center">
+          <TouchableWithoutFeedback onPress={onTextClick}>
+            <Text
+              className="text-white"
+              style={{
+                fontFamily: FontInter.semiBold,
+                fontSize: Dimensions.get("screen").width * 0.2,
+                // width: Dimensions.get("screen").width * 0.3,
+                // height: 200,
+                textAlign: "center",
+              }}
+            >
+              {formatTime(number, false)}
+            </Text>
+          </TouchableWithoutFeedback>
+          <TextInput
+            ref={textInput}
+            style={{ display: "none" }}
+            onChangeText={onChangeText}
+            value={number.toString()}
+            keyboardType="numeric"
+          />
+        </View>
+      </TouchableWithoutFeedback>
+    </ScreenWrapper>
   );
 }
--- utils/time.ts
+++ utils/time.ts
@@ -9,16 +9,44 @@
  * @generate-docs
  */
 
-export const formatTime = (timer: number) => {
+export const formatTime = (timer: number, hoursActive = true) => {
     const getSeconds = `0${(timer % 60)}`.slice(-2)
     const minutes = Math.floor(timer / 60)
     const getMinutes = `0${minutes % 60}`.slice(-2)
     const hours = Math.floor(timer / 3600)
 
-    if (hours) {
+    if (hoursActive && hours) {
         const getHours = `0${hours}`.slice(-2)
         return `${getHours} : ${getMinutes} : ${getSeconds}`
     }
+
+    return `${getMinutes} : ${getSeconds}`
+}
+
+export const formatTimeNumber = (timer: number) => {
+    const seconds = timer % 100;
+    const getSeconds = `0${seconds}`.slice(-2)
+    const minutes = Math.floor(timer / 100)
+    const getMinutes = `0${minutes}`.slice(-2)
+
+    return `${getMinutes} : ${getSeconds}`
+}
+
+export const reformatTimeNumber = (timer: number) => {
+    const seconds = timer % 100;
+    const reelSeconds = seconds % 60;
+    const minutes = Math.floor(timer / 100) + Math.floor(seconds / 60);
+    const reelMinutes = minutes > 60 ? 60 : minutes;
+    if (timer >= 6000)
+        return 6000;
+    const reelNumber = Number(`${reelMinutes}${reelSeconds}`);
+
+    return reelNumber;
+}
+
+export const formatTimeString = (timer: string) => {
+    const getSeconds = timer.slice(0, 2)
+    const getMinutes = timer.slice(2)
 
     return `${getMinutes} : ${getSeconds}`
 }
