4f7aa5d02f7b03d31a4efb276070fe3d35a8d936
Pierre Véron <pierre.veron@outlook.fr>
2021-11-21 10:43:04 +0100

    added optional callback on fade

--- hooks/useFade.ts
+++ hooks/useFade.ts
@@ -5,20 +5,24 @@ import { Animated } from "react-native";
 export default function useFade(initialValue: number, fadeDuration: number, fadeOutDuration?: number) {
     const fadeAnim = useRef(new Animated.Value(initialValue)).current;
 
-    const fadeIn = () => {
+    const fadeIn = (callback?: () => void) => {
         Animated.timing(fadeAnim, {
             toValue: 1,
             duration: fadeDuration,
             useNativeDriver: false,
         }).start();
+        if (callback)
+            setTimeout(callback, fadeOutDuration ?? fadeDuration);
     };
 
-    const fadeOut = () => {
+    const fadeOut = (callback?: () => void) => {
         Animated.timing(fadeAnim, {
             toValue: 0,
             duration: fadeOutDuration ?? fadeDuration,
             useNativeDriver: false,
         }).start();
+        if (callback)
+            setTimeout(callback, fadeOutDuration ?? fadeDuration);
     };
 
     return { fadeAnim, fadeIn, fadeOut }
