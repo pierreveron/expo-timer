40d9676d51159c2c8ac8db917cafce8bb954bb00
Pierre Véron <pierre.veron@outlook.fr>
2021-11-21 16:03:31 +0100

    refactored classic timer web

--- screens/ClassicTimerScreen.tsx
+++ screens/ClassicTimerScreen.tsx
@@ -1,13 +1,14 @@
 import React, { useEffect } from "react";
-import { Dimensions } from "react-native";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
+import BottomButtons from "../components/BottomButtons";
 import BouncingText from "../components/BoucingText";
-import CustomButton from "../components/Button";
+import FadedView from "../components/FadedView";
+import FinishedText from "../components/FinishedText";
 import ScreenWrapper from "../components/ScreenWrapper";
-import { View, Text } from "../components/tailwind";
+import { View } from "../components/tailwind";
 import TimeText from "../components/TimeText";
 import Colors from "../constants/Colors";
-import useTimer from "../hooks/useClassicTimer";
+import FadeDuration from "../constants/FadeDuration";
+import useClassicTimer from "../hooks/useClassicTimer";
 import { RootStackScreenProps } from "../types";
 
 export default function ClassicTimerScreen({
@@ -17,13 +18,14 @@ export default function ClassicTimerScreen({
     timer,
     isActive,
     isPaused,
+    isFinished,
     handleStart,
     handlePause,
     handleResume,
     handleReset,
     upTimer,
     downTimer,
-  } = useTimer(1800);
+  } = useClassicTimer(1800);
 
   var throttle: NodeJS.Timer | null = null;
   const throttleDuration = 80;
@@ -54,7 +56,6 @@ export default function ClassicTimerScreen({
     }
   }, [isActive, isPaused]);
 
-  const { bottom: insetsBottom } = useSafeAreaInsets();
   return (
     <ScreenWrapper
       title="Classic Timer"
@@ -62,35 +63,25 @@ export default function ClassicTimerScreen({
       backgroundColor={Colors.classicTimer}
     >
       <View className="h-full items-center justify-center">
-        <TimeText>{timer}</TimeText>
-        <View
-          style={{
-            position: "absolute",
-            bottom: insetsBottom,
-          }}
+        <FinishedText visible={isFinished} fadeDuration={FadeDuration} />
+        <FadedView visible={!isFinished} fadeDuration={FadeDuration}>
+          <TimeText big>{timer}</TimeText>
+        </FadedView>
+        <BouncingText
+          visible={!isActive && !isPaused}
+          fadeDuration={FadeDuration}
         >
-          {isActive && !isPaused && (
-            <CustomButton
-              title="Reset"
-              onPress={handleReset}
-              style={{ marginBottom: insetsBottom / 2 }}
-            />
-          )}
-          {!isActive && !isPaused ? (
-            <>
-              <BouncingText>Scroll up to add time, down to reduce</BouncingText>
-              <CustomButton
-                title="Start"
-                onPress={handleStart}
-                style={{ marginTop: Dimensions.get("screen").height / 8 }}
-              />
-            </>
-          ) : isPaused ? (
-            <CustomButton title="Stop" onPress={handlePause} />
-          ) : (
-            <CustomButton title="Resume" onPress={handleResume} />
-          )}
-        </View>
+          Swipe up to add time, down to reduce
+        </BouncingText>
+        <BottomButtons
+          isActive={isActive}
+          isPaused={isPaused}
+          isFinished={isFinished}
+          handleReset={handleReset}
+          handleStart={handleStart}
+          handlePause={handlePause}
+          handleResume={handleResume}
+        />
       </View>
     </ScreenWrapper>
   );
