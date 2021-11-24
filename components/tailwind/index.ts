00bc71f69574f77f286db24f6313cfabd226bac4
Pierre Véron <pierre.veron@outlook.fr>
2021-11-24 19:46:24 +0000

    added FadedView to use tailwind

--- components/tailwind/index.ts
+++ components/tailwind/index.ts
@@ -1,5 +1,6 @@
 import * as RN from "react-native";
 import { withTailwind } from "./hoc";
+import FV from "../FadedView";
 
 export const View = withTailwind(RN.View);
 export const Text = withTailwind(RN.Text);
@@ -7,3 +8,4 @@ export const TouchableOpacity = withTailwind(RN.TouchableOpacity);
 export const TouchableHighlight = withTailwind(RN.TouchableHighlight);
 export const TextInput = withTailwind(RN.TextInput)
 export const Image = withTailwind(RN.Image)
+export const FadedView = withTailwind(FV)
